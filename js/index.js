f21e101a136f2b534aa4b18d0c70d162393c7ba2
Meng Wang <water.wang@live.com>
2017-03-22 22:47:05 -0500

    update

--- js/index.js
+++ js/index.js
@@ -15,25 +15,33 @@ let main = new Vue({
     },
     methods: {
         donate: function () {
-            Ca$.get({
-                url: './php/tget.php',
-                data: {
-                    test: 'test'
-                },
-                success: function (data) {
-                    console.log('test' + data);
-                }
-            });
-            Ca$.post({
-                url: './php/tpost.php',
-                data: {
-                    test: 'test'
-                },
-                success: function (data) {
-                    console.log('test' + data);
-                }
-            });
-
+            // Ca$.get({
+            //     url: './php/tget.php',
+            //     data: {
+            //         test: 'test'
+            //     },
+            //     success: function (data) {
+            //         console.log('test' + data);
+            //     }
+            // });
+            // Ca$.post({
+            //     url: './php/tpost.php',
+            //     data: {
+            //         test: 'test'
+            //     },
+            //     success: function (data) {
+            //         console.log('test' + data);
+            //     }
+            // });
+        },
+        reimport: function () {
+            window.location.href = './rev/?mode=import';
+        },
+        recreate: function () {
+            window.location.href = './create/?mode=create';
+        },
+        remcode: function () {
+            window.location.href = './rev/?mode=mcode';
         }
     }
 })
---
+++ js/mcode.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+}());
+/*
+ * @author WMXPY
+ * @contect wm@wmpcxpy.com
+ */
+jQuery(document).ready(function () {
+    $.backstretch("../imgs/indexbg.jpg");
+});
+
+console.log(getURLVar('mode'));
+
+function getURLVar(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+}
