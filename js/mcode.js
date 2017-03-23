23592102025085a89421f048cd18d5d1b9024dea
WMXPY <wm@wmpcxpy.com>
2017-03-23 15:56:02 -0500

    update

--- js/mcode.js
+++ js/mcode.js
@@ -18,7 +18,8 @@ let title = new Vue({
     data: {
         mode: getURLVar('mode'),
         testname: '',
-        tname: ''
+        tname: '',
+        questions: []
     },
     computed: {
         topofpage: function () {
@@ -36,8 +37,65 @@ let title = new Vue({
 
         }
     }
+});
+let app = new Vue({
+    el: '#main',
+    data: {
+        tests: []
+    },
+    methods: {
+        add: function (question, correct, a, b, c, d, e) {
+            var showc = 'hidden';
+            var showd = 'hidden';
+            var showe = 'hidden';
+            if (c.length > 0) {
+                showc = 'hiddens';
+            }
+            if (d.length > 0) {
+                showd = 'hiddens';
+            }
+            if (e.length > 0) {
+                showe = 'hiddens';
+            }
+            var thatquestion = {
+                question: question,
+                a: a,
+                b: b,
+                c: c,
+                showc: showc,
+                d: d,
+                showd: showd,
+                e: e,
+                showe: showe,
+                sol: [false, false, false, false, false],
+                correct: correct - 1,
+                correctorwrong: '-'
+            }
+            this.tests.push(thatquestion);
+        },
+        grade: function () {
+            for (var i = 0; i < this.tests.length; i++) {
+                if (this.tests[i].sol[this.tests[i].correct] == true) {
+                    this.tests[i].correctorwrong = 'Correct';
+                } else {
+                    this.tests[i].correctorwrong = 'Wrong';
+                }
+            }
+        },
+        gradethis: function (i) {
+            if (i.sol[i.correct] == true) {
+                i.correctorwrong = 'Correct';
+            } else {
+                i.correctorwrong = 'Wrong';
+            }
+        }
+    }
 })
 
 function getURLVar(name) {
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
 }
+app.add('1.9 Related to Theater as Seeing Place, Elizabethan theatre performers thought of the stage as a:', 1,
+    'Mirror ', 'Pedestal ', 'A time machine ', 'a place of wealth', '');
+app.add('1.10 My instructor for this course is', 3, 'Jim Trenberth ', 'Stephen Leath',
+    'Kelly Marie Schaefer ', 'I have no idea', '');
