a1d46a201a0d943e12daf14c95e6741d0c404187
Meng Wang <water.wang@live.com>
2017-03-28 16:37:58 -0500

    componenets

--- js/mcode.js
+++ js/mcode.js
@@ -10,7 +10,9 @@ jQuery(document).ready(function () {
     if (getURLVar('mode') != null) title.mode = getURLVar('mode');
     $.backstretch("../imgs/indexbg.jpg");
 });
-
+Vue.component('re-credit', {
+    template: '<div><p style="color:#565656"><i class="fa fa-code"></i> Review.md with <i class="fa fa-heart"></i> by WMXPY@<a href="http://mengw.io">mengw.io</a> 2016</p></div>'
+});
 let title = new Vue({
     el: '#title',
     data: {
--- js/ob.js
+++ js/ob.js
@@ -8,3 +8,6 @@
 jQuery(document).ready(function () {
     $.backstretch("../../imgs/indexbg.jpg");
 });
+Vue.component('re-credit', {
+    template: '<div><p style="color:#565656"><i class="fa fa-code"></i> Review.md with <i class="fa fa-heart"></i> by WMXPY@<a href="http://mengw.io">mengw.io</a> 2016</p></div>'
+});
